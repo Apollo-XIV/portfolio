27fe80a2101adc6fa8ae6968e9d87829ffa5fbce
asdf <asdf@asdf>
2023-09-13 01:11:59 +0100

    tweaks

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# portfolio
+# portfolio website
+
+
+
+
+# Dev Instructions:
+
+to run tailwind cli:
+```
+✦ ❯ npx tailwindcss -i ./input.css -o ./public/tailwind.css --watch
+```
+
+to run dioxus hr server:
+```
+✦ ❯ dx serve --hot-reload
+```
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -26,14 +26,46 @@ fn App(cx: Scope) -> Element {
                 p {"Hey, that's pretty cool, right?"},
                 p {"What else can we do? Here's a component with something fancy, maybe."},
                 CoolComponent {}
-                
+                CoolerComponent {}
             }
         }
     })
 }
 
 
+fn CoolerComponent(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    render! {
+        div {
+            class: "mt-4 p-4 w-full h-24 bg-white rounded-xl text-black flex place-items-center justify-center gap-2 flex-wrap",
+            h1 {class: "w-full","OMG LOOK ITS A COUNTER!"}
+            div {
+                class: "w-10 h-10 outline cursor-pointer outline-gray-500 outline-2 rounded-lg",
+                onclick: move |_| {
+                    count -= 1
+                },
+                h1 {class: "text-center text-3xl select-none","-"}
+            },
+            div {
+                class: "w-10 h-10 outline cursor-pointer outline-gray-500 outline-2 rounded-lg",
+                h1 {class: "text-center text-3xl","{count}"}
+            },
+            div {
+                class: "w-10 h-10 outline cursor-pointer outline-gray-500 outline-2 rounded-lg select-none",
+                onclick: move |_| {
+                    count += 1
+                },
+                h1 {class: "text-center text-3xl","+"}
+            },
+        }
+    }
+}
+
+
 fn CoolComponent(cx: Scope) -> Element {
+
+
     render! {
         div {
             class: "mt-2 p-4 w-full h-24 bg-white rounded-xl",
